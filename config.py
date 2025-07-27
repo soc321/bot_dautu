163c35b0467f639469349a5a630069eb61deba63
soc321 <91270627+soc321@users.noreply.github.com>
2025-07-27 15:08:37 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = "YOUR_BOT_TOKEN"
+ADMIN_IDS = ["123456789"]  # Thay bằng Telegram ID của bạn
+BOT_BANK_NAME = "MB BANK"
+BOT_BANK_NUMBER = "1234567890"
+MIN_WITHDRAW = 7000
+MAX_WITHDRAW = 500000
+
+INVESTMENTS = [
+    {"name": "Gói 1", "amount": 10000, "daily": 2000, "days": 5},
+    {"name": "Gói 2", "amount": 50000, "daily": 12000, "days": 7},
+    {"name": "Gói 3", "amount": 100000, "daily": 25000, "days": 10},
+    {"name": "Gói 4", "amount": 200000, "daily": 60000, "days": 15},
+]
